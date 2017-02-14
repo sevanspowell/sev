9c49507570863cc228154acdaa261733500c98aa
Samuel Evans-Powell <samueldavid.ep@gmail.com>
2017-02-14 20:07:41 +0800

    Improve SDL2Platform unit tests
    Created window in SDL2Platform unit test so I can test the capture of mouse
    movement and mouse button events.

--- src/svLibrary/test/test_sdl2platform.h
+++ src/svLibrary/test/test_sdl2platform.h
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <memory>
 
+#include <SDL2/SDL.h>
+
 #include <sv/platform/SDL2Platform.h>
 
 // Test we can initialize and use SDL2Platform to get the number of milliseconds
@@ -14,46 +16,67 @@ TEST(SDL2Platform, GetMilliseconds) {
 }
 
 // Example event loop testing 'getNextEvent'
-TEST(SDL2Platform, ExampleEventLoop) {
-    sv::SDL2Platform platform;
+// TEST(SDL2Platform, ExampleEventLoop) {
+//     sv::SDL2Platform platform;
 
-    EXPECT_TRUE(platform.initialize());
+//     EXPECT_TRUE(platform.initialize());
 
-    while (true) {
-        sv::PlatformEvent event;
-        while (platform.getNextEvent(&event)) {
-            std::cout << "An event occurred at time: " << event.time
-                      << std::endl;
+//     SDL_InitSubSystem(SDL_INIT_VIDEO);
 
-            switch (event.type) {
-            case sv::PlatformEventType::Key: {
-                std::cout << "\tKey event: "
-                          << "'" << sv::getKeycodeString(
-                                        (sv::Keycodes::Enum)event.value1)
-                          << "' went " << (event.value2 == 1 ? "Down" : "Up")
-                          << "." << std::endl;
-                break;
-            }
-            case sv::PlatformEventType::Mouse: {
-                std::cout << "\tMouse event: mouse moved x: " << event.value1
-                          << " y: " << event.value2 << "." << std::endl;
-                break;
-            }
-            case sv::PlatformEventType::Quit: {
-                std::cout << "\tQuit event." << std::endl;
-                break;
-            }
-            default: {
-                std::cout << "\tUnkown event." << std::endl;
-                break;
-            }
-            }
-        }
+//     SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+//     SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+//     SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+//     SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
 
-        if (event.type == sv::PlatformEventType::Quit) {
-            break;
-        }
-    }
+//     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+//     SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
 
-    platform.shutdown();
-}
+//     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+//     uint32_t flags = 0x0;
+//     flags |= SDL_WINDOW_OPENGL;
+//     flags |= SDL_WINDOW_RESIZABLE;
+
+//     // Create window
+//     SDL_Window *window =
+//         SDL_CreateWindow("Test bed", SDL_WINDOWPOS_CENTERED,
+//                          SDL_WINDOWPOS_CENTERED, 800, 600, flags);
+
+//     while (true) {
+//         sv::PlatformEvent event;
+//         while (platform.getNextEvent(&event)) {
+//             std::cout << "An event occurred at time: " << event.time
+//                       << std::endl;
+
+//             switch (event.type) {
+//             case sv::PlatformEventType::Key: {
+//                 std::cout << "\tKey event: "
+//                           << "'" << sv::getKeycodeString(
+//                                         (sv::Keycodes::Enum)event.value1)
+//                           << "' went " << (event.value2 == 1 ? "Down" : "Up")
+//                           << "." << std::endl;
+//                 break;
+//             }
+//             case sv::PlatformEventType::Mouse: {
+//                 std::cout << "\tMouse event: mouse moved x: " << event.value1
+//                           << " y: " << event.value2 << "." << std::endl;
+//                 break;
+//             }
+//             case sv::PlatformEventType::Quit: {
+//                 std::cout << "\tQuit event." << std::endl;
+//                 break;
+//             }
+//             default: {
+//                 std::cout << "\tUnkown event." << std::endl;
+//                 break;
+//             }
+//             }
+//         }
+
+//         if (event.type == sv::PlatformEventType::Quit) {
+//             break;
+//         }
+//     }
+
+//     platform.shutdown();
+// }
